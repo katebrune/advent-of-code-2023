2a998f82fb642ad5ee7f08cc4533cb5800634c57
Kate Brune <kbrune@vailsys.com>
2023-12-13 10:47:01 -0600

    day03 part2

--- day03.py
+++ day03.py
@@ -28,3 +28,49 @@ with open("./data/day03.txt", "r") as f:
             temp_row = temp_row[0: temp_row.find(
                 number)] + '.'*len(number) + temp_row[temp_row.find(number) + len(number):]
     print(sum)
+
+"""
+Part 2
+"""
+with open("./data/day03.txt", "r") as f:
+    data = []
+    for line in f:
+        data.append([*line.strip()])
+    sum = 0
+    for i, row in enumerate(data):
+        for j, value in enumerate(row):
+            if re.match('\*', value):
+                neighbors = []
+                if i > 0 and re.match('[0-9]', data[i-1][j]):
+                    left = re.search(r'\d+$', ''.join(data[i-1][0:j]))
+                    right = re.search(r'^\d+', ''.join(data[i-1][j+1:]))
+                    neighbors.append("{}{}{}".format(
+                        left.group(0) if left != None else "", data[i-1][j], right.group(0) if right != None else ""))
+                else:
+                    if i > 0 and j > 1 and re.match('[0-9]', data[i-1][j-1]):
+                        left = re.search(r'\d+$', ''.join(data[i-1][0:j]))
+                        neighbors.append("{}".format(left.group(0)))
+                    if i > 0 and j < len(data[i]) and re.match('[0-9]', data[i-1][j + 1]):
+                        right = re.search(r'^\d+', ''.join(data[i-1][j+1:]))
+                        neighbors.append("{}".format(right.group(0)))
+                if j > 0 and re.match('[0-9]', data[i][j-1]):
+                    left = re.search(r'\d+$', ''.join(data[i][0:j]))
+                    neighbors.append('{}'.format(left.group(0)))
+                if j < len(data[i]) and re.match('[0-9]', data[i][j+1]):
+                    right = re.search(r'^\d+', ''.join(data[i][j+1:]))
+                    neighbors.append("{}".format(right.group(0)))
+                if i < len(data) and re.match("[0-9]", data[i+1][j]):
+                    left = re.search(r'\d+$', ''.join(data[i+1][0:j]))
+                    right = re.search(r'^\d+', ''.join(data[i+1][j+1:]))
+                    neighbors.append("{}{}{}".format(
+                        left.group(0) if left != None else "", data[i+1][j], right.group(0) if right != None else ""))
+                else:
+                    if i < len(data) and j > 1 and re.match('[0-9]', data[i+1][j-1]):
+                        left = re.search(r'\d+$', ''.join(data[i+1][0:j]))
+                        neighbors.append("{}".format(left.group(0)))
+                    if i < len(data) and j < len(data[i]) and re.match('[0-9]', data[i+1][j+1]):
+                        right = re.search(r'^\d+', ''.join(data[i+1][j+1:]))
+                        neighbors.append("{}".format(right.group(0)))
+                if (len(neighbors) == 2):
+                    sum += int(neighbors[0])*int(neighbors[1])
+    print(sum)
